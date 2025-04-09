23eaa0812e5cd756bf2b5d725a5f326794cecb6f
Asimo <rvolenc@gmail.com>
2025-04-09 11:39:26 +0400

    feat: implement adding tasks to the list

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -10,34 +10,58 @@ interface Task {
 
 const Todo: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
+  const [taskList, setTaskList] = useState<Task[]>([]);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Здесь добавить логику добавления задачи
+
+    if (inputValue.trim()) {
+      setTaskList((prev) => [
+        ...prev,
+        // for id I would prefer to use crypto.randomUUID(), but since the interface specifies the type number, the generation is based on the date
+        {
+          id: Date.now(),
+          text: inputValue,
+          completed: false,
+        },
+      ]);
+    }
+
     setInputValue("");
   };
 
   return (
-    <div className='todo-container'>
-      <div className='todo-header'>
+    <div className="todo-container">
+      <div className="todo-header">
         <h1>Список задач</h1>
         <ThemeToggle />
       </div>
 
-      <form className='input-section' onSubmit={handleSubmit}>
+      <form className="input-section" onSubmit={handleSubmit}>
         <input
-          type='text'
-          className='task-input'
-          placeholder='Введите новую задачу'
+          type="text"
+          className="task-input"
+          placeholder="Введите новую задачу"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <button type='submit' className='add-button'>
+        <button type="submit" className="add-button">
           Добавить
         </button>
       </form>
 
-      <div className='tasks-list'>{/* Здесь должны отображаться задачи */}</div>
+      <div className="tasks-list">
+        <ul>
+          {taskList.map((task) => (
+            <li key={task.id} className="task-item">
+              <label>
+                <input type="checkbox" />
+                {task.text}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
