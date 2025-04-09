aecec2b911b48e1449b1b3f80aac4cb11c853a3d
Asimo <rvolenc@gmail.com>
2025-04-09 15:19:21 +0400

    feat: implement switching between dark and light themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Todo from "./components/Todo";
 import "./App.scss";
+import { ThemeContext } from "./context/ThemeContext";
 
 const App: React.FC = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className='App'>
+    <div className="App" data-theme={theme}>
       <Todo />
     </div>
   );
---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, ReactNode, useState } from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
